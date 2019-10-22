7e5668bbd7cea0d273a2d2ac1a52dd4e22c3646f
Lazar Otasevic <redhotbits@gmail.com>
2019-10-23 00:01:28 +0200

    try without platforms

--- Package.swift
+++ Package.swift
@@ -3,9 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "RHBKit",
-    platforms: [
-        .macOS(.v10_12), .iOS("10.3")
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
