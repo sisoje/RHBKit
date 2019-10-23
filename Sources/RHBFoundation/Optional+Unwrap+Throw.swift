67eebe118680dd4d67f4342118cace6021cf7220
Lazar Otasevic <redhotbits@gmail.com>
2019-10-23 13:25:34 +0200

    renamed methods to be less common

--- Sources/RHBFoundation/Optional+Unwrap+Throw.swift
+++ Sources/RHBFoundation/Optional+Unwrap+Throw.swift
@@ -1,15 +1,15 @@
 import Foundation
 
 public extension Optional {
-    func unwrap(file: String = #file, line: Int = #line, function: String = #function) throws -> Wrapped {
+    func forceUnwrap(file: String = #file, line: Int = #line, function: String = #function) throws -> Wrapped {
         guard let result = self else {
             throw CodeLocationInfo("Can not unwrap to \(String(describing: Wrapped.self))", file: file, line: line, function: function)
         }
         return result
     }
 
-    func cast<T: Any>(_ type: T.Type, file: String = #file, line: Int = #line, function: String = #function) throws -> T {
-        let unwraped = try unwrap(file: file, line: line, function: function)
+    func forceCast<T: Any>(as type: T.Type, file: String = #file, line: Int = #line, function: String = #function) throws -> T {
+        let unwraped = try forceUnwrap(file: file, line: line, function: function)
         guard let result = unwraped as? T else {
             throw CodeLocationInfo("Can not cast \(String(describing: unwraped)) to \(String(describing: type))", file: file, line: line, function: function)
         }
