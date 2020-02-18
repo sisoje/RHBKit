e47908a48840e88122818b3ef324ac41b8c443a0
Lazar Otasevic <redhotbits@gmail.com>
2020-02-19 00:02:53 +0100

    making core data as monorepo

---
+++ RHBCoreData/Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "RHBCoreData",
+    platforms: [
+        .macOS(.v10_12), .iOS("10.3")
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "RHBCoreData",
+            targets: ["RHBCoreData"]
+        ),
+        .library(
+            name: "RHBCoreDataTestUtilities",
+            targets: ["RHBCoreDataTestUtilities"]
+        )
+    ],
+    dependencies: [
+        .package(path: "../RHBFoundation")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "RHBCoreData",
+            dependencies: ["RHBFoundation"],
+            path: "Sources"
+        ),
+        .target(
+            name: "RHBCoreDataTestUtilities",
+            dependencies: ["RHBFoundation", "RHBCoreData"],
+            path: "TestUtilities"
+        ),
+        .testTarget(
+            name: "RHBCoreDataTests",
+            dependencies: ["RHBFoundation", "RHBCoreData", "RHBCoreDataTestUtilities"],
+            path: "Tests"
+        )
+    ]
+)
